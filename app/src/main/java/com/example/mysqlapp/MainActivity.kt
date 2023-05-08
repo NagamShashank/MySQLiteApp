3929228e1eadbd6d5e7c5b14f689f2669c7fc451
Shashank Nagam <shashanknagam@nimapinfotech.com>
2023-05-08 18:18:17 +0530

    Second Commit

--- app/src/main/java/com/example/mysqlapp/MainActivity.kt
+++ app/src/main/java/com/example/mysqlapp/MainActivity.kt
@@ -43,7 +43,6 @@ class MainActivity : AppCompatActivity() {
         binding.DataRecyclerView.adapter = adapter
 
     }
-
     private fun addData() {
 
         val dpList = dbHelper.getAllUsers()
